3a54e6535935d981da8ae1be37292840968b3efb
pavan <88082839+hatakeNDP@users.noreply.github.com>
2024-12-13 20:14:28 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_07CFLdJJp', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1mdfjff3m7ci4h1bp6v5o416l5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
