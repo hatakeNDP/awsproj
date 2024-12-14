1f176aae63cd463ca7ee929dcfd9c55ac4d9d514
pavan <88082839+hatakeNDP@users.noreply.github.com>
2024-12-13 20:31:11 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_07CFLdJJp', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1mdfjff3m7ci4h1bp6v5o416l5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '13dm3qrd2i36j5t36j5i8nth7i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
